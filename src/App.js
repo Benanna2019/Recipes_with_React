1dedecb81186cd68ac1ffeab44e6347045009bfa
Benanna2019 <bass41992ben@gmail.com>
2020-09-30 17:25:03 -0400

    created way of displaying recipes w/o doing anything with state.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { useState } from "react";
-import Counter from "./components/Counter";
-
-export const ThemeContext = React.createContext();
+import React from "react";
+import RecipeList from "./RecipeList";
 
 function App() {
-  const [theme, setTheme] = useState("red");
   return (
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      <Counter initialCount={0} />
-      <button
-        onClick={() =>
-          setTheme((prevTheme) => {
-            return prevTheme === "red" ? "blue" : "red";
-          })
-        }
-      >
-        ToggleTheme
-      </button>
-    </ThemeContext.Provider>
+    <div>
+      <RecipeList recipes={sampleRecipes} />
+    </div>
   );
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: "Plain Chicken",
+    servings: 3,
+    cookTime: "1:45",
+    instructions:
+      "1. Put salt on Chicken \n2. Put Chicken in Oven \n3. Eat Chicken",
+  },
+  {
+    id: 1,
+    name: "Plain Pork",
+    servings: 5,
+    cookTime: "0:45",
+    instructions: "1. Put paprika on Pork \n2. Put Pork in Oven \n3. Eat Pork",
+  },
+];
 
 export default App;
