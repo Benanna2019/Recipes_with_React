45780aad94a0b9f910d6725b2dfe929f8a444870
Benanna2019 <bass41992ben@gmail.com>
2020-09-30 17:54:33 -0400

    created ingredients components

--- src/App.js
+++ src/App.js
@@ -17,6 +17,18 @@ const sampleRecipes = [
     cookTime: "1:45",
     instructions:
       "1. Put salt on Chicken \n2. Put Chicken in Oven \n3. Eat Chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: "Chicken",
+        amount: "2 pounds",
+      },
+      {
+        id: 2,
+        name: "Salt",
+        amount: "2 Tbsp",
+      },
+    ],
   },
   {
     id: 1,
@@ -24,6 +36,18 @@ const sampleRecipes = [
     servings: 5,
     cookTime: "0:45",
     instructions: "1. Put paprika on Pork \n2. Put Pork in Oven \n3. Eat Pork",
+    ingredients: [
+      {
+        id: 1,
+        name: "Pork",
+        amount: "3 pounds",
+      },
+      {
+        id: 2,
+        name: "Paprika",
+        amount: "2 Tbsp",
+      },
+    ],
   },
 ];
 
